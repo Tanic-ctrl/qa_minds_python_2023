ca322a3e4f41aefbce9d08927cb1c54c2fe705da
thanii <taniaradillac@gmail.com>
2023-02-25 08:09:37 -0600

    practica 05

---
+++ python_06_01.py
@@ -0,0 +1,44 @@
+edad_persona = input("Ingrese su edad:")
+print(edad_persona)
+
+estatura_persona = input("Ingrese su estatura en centimetros:")
+print(estatura_persona)
+
+if edad_persona > 14:
+    return true
+
+
+
+if edad_persona and estatura_persona
+
+boleto_avion = True
+passaporte = True
+tres_vacunas_covid = False
+prueba_pcr = False
+
+if edad_persona and passaporte and (tres_vacunas_covid or prueba_pcr):
+    print("Abordar")
+else:
+    print("No abordar")
+
+
+#CONSTANTS
+AGE_LIMIT = 14
+HEIGHT_LIMIT = 1.62
+
+#USER INPUTS
+age = input ("age")
+age = int(age)
+height = input( "height")
+height = float (height)
+
+
+#LOGIC
+
+valid_age = age > AGE_LIMIT
+valid_height = height > HEIGHT_LIMIT
+
+if valid_height and valid_height:
+    print ("Can aboard")
+    else
+    print ()
---
+++ python_06_02.py
@@ -0,0 +1,15 @@
+anio_actual = 2023
+mes_actual = 2
+
+user_day = input("Ingrese su dia de nacimiento:")
+
+user_month = input("Ingrese su mes de nacimiento:")
+
+user_year= input("Ingrese su anio de nacimiento:")
+
+user_year = int(user_year)
+anio_actual = int(anio_actual)
+
+user_age = anio_actual - user_year
+print(user_age)
+
